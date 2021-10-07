a731bb5ab6ad85927bcaf8c3906a61b9d212a53c
EstebanHincapieVasquez <maicolhincapie200104@correo.itm.edu.co>
2021-10-07 17:39:38 -0500

    Begin users

--- lib/helpers/api_helper.dart
+++ lib/helpers/api_helper.dart
@@ -5,9 +5,10 @@ import 'package:http/http.dart' as http;
 
 import 'package:vehicles_app/models/brand.dart';
 import 'package:vehicles_app/models/document_type.dart';
-import 'package:vehicles_app/models/vehicle_type.dart';
 import 'package:vehicles_app/models/procedure.dart';
 import 'package:vehicles_app/models/response.dart';
+import 'package:vehicles_app/models/user.dart';
+import 'package:vehicles_app/models/vehicle_type.dart';
 
 class ApiHelper {
 
@@ -65,33 +66,6 @@ class ApiHelper {
     return Response(isSuccess: true, result: list);
   }
 
-  static Future<Response> getVehicleTypes(String token) async {
-    var url = Uri.parse('${Constants.apiUrl}/api/VehicleTypes');
-    var response = await http.get(
-      url,
-      headers: {
-        'content-type' : 'application/json',
-        'accept' : 'application/json',
-        'authorization': 'bearer $token',
-      },
-    );
-
-    var body = response.body;
-    if (response.statusCode >= 400) {
-      return Response(isSuccess: false, message: body);
-    }
-
-    List<VehicleType> list = [];    
-    var decodedJson = jsonDecode(body);
-    if (decodedJson != null) {
-      for (var item in decodedJson) {
-        list.add(VehicleType.fromJson(item));
-      }
-    }
-
-    return Response(isSuccess: true, result: list);
-  }
-
   static Future<Response> getProcedures(String token) async {
     var url = Uri.parse('${Constants.apiUrl}/api/Procedures');
     var response = await http.get(
@@ -119,6 +93,60 @@ class ApiHelper {
     return Response(isSuccess: true, result: list);
   }
 
+  static Future<Response> getUsers(String token) async {
+    var url = Uri.parse('${Constants.apiUrl}/api/Users');
+    var response = await http.get(
+      url,
+      headers: {
+        'content-type' : 'application/json',
+        'accept' : 'application/json',
+        'authorization': 'bearer $token',
+      },
+    );
+
+    var body = response.body;
+    if (response.statusCode >= 400) {
+      return Response(isSuccess: false, message: body);
+    }
+
+    List<User> list = [];    
+    var decodedJson = jsonDecode(body);
+    if (decodedJson != null) {
+      for (var item in decodedJson) {
+        list.add(User.fromJson(item));
+      }
+    }
+
+    return Response(isSuccess: true, result: list);
+  }
+
+  static Future<Response> getVehicleTypes(String token) async {
+    var url = Uri.parse('${Constants.apiUrl}/api/VehicleTypes');
+    var response = await http.get(
+      url,
+      headers: {
+        'content-type' : 'application/json',
+        'accept' : 'application/json',
+        'authorization': 'bearer $token',
+      },
+    );
+
+    var body = response.body;
+    if (response.statusCode >= 400) {
+      return Response(isSuccess: false, message: body);
+    }
+
+    List<VehicleType> list = [];    
+    var decodedJson = jsonDecode(body);
+    if (decodedJson != null) {
+      for (var item in decodedJson) {
+        list.add(VehicleType.fromJson(item));
+      }
+    }
+
+    return Response(isSuccess: true, result: list);
+  }
+  
   static Future<Response> put(String controller, String id, Map<String, dynamic> request, String token) async {
     var url = Uri.parse('${Constants.apiUrl}$controller$id');
     var response = await http.put(
